af439ed6f12b501335d3ae953368f3b25b08c1fb
asapognikov <a.sapognikov.barvihalife@yandex.ru>
2022-09-06 01:10:34 +0500

    create second project v2.0

--- Project002_HelloUser/Program.cs
+++ Project002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(username);
