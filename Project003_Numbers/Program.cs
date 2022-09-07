b0014483ab32760071b84915c901ffd8d3cf2a04
asapognikov <a.sapognikov.barvihalife@yandex.ru>
2022-09-07 23:57:54 +0500

    Создали программу сложения 2 целых чисел

--- Project003_Numbers/Program.cs
+++ Project003_Numbers/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
